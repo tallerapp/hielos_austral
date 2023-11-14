2451dbd3596e00c525241339d8f1aa4e2eccfccd
tallerapp <grupotallerapp@gmail.com>
2023-11-14 17:02:43 -0300

    creacion de modelos

--- modelapp/models.py
+++ modelapp/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+class Cliente(models.Model):
+    cliente_id = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=50)
+    apellidos = models.CharField(max_length=50)
+    correo_electronico = models.EmailField()
+    password = models.CharField(max_length=100)
+
+
+class Cabaña(models.Model):
+    cabaña_id = models.AutoField(primary_key=True)
+    precio = models.DecimalField(max_digits=10, decimal_places=2)
+    capacidad = models.PositiveIntegerField()
+    descripcion = models.TextField()
+    imagen = models.ImageField(upload_to='cabañas/')
+    
+# hay que descargar una libreria pip install Pillow para trabajar con imagenes
+
+class Reserva(models.Model):
+    reserva_id = models.AutoField(primary_key=True)
+    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
+    cabaña = models.ForeignKey(Cabaña, on_delete=models.CASCADE)
+    fecha_inicio = models.DateField()
+    fecha_fin = models.DateField()
+
+class Disponibilidad(models.Model):
+    fecha = models.DateField()
+    cabaña = models.ForeignKey(Cabaña, on_delete=models.CASCADE)
+    disponible = models.BooleanField(default=True)
+
