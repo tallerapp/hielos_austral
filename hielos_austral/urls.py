a394aac552b9fc45d10fc0ffbb352259c85181d6
tallerapp <grupotallerapp@gmail.com>
2023-11-15 14:38:23 -0300

    cabañas de prueba

--- hielos_austral/urls.py
+++ hielos_austral/urls.py
@@ -14,11 +14,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path
 from modelapp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.inicio),
+    path('',views.inicio),
+    path('cabañas/', views.cabañas),
 ]
+if settings.DEBUG:
+    from django.conf.urls.static import static
+    urlpatterns += static(settings.MEDIA_URL, document_root =settings.MEDIA_ROOT)
