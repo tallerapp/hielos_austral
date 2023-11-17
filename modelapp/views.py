efc1c3c3283b6f5ae2cd56f5096cc985de687527
Javier <devgallar@gmail.com>
2023-11-17 16:51:13 -0400

    creacion del nav,  imagen principal de la pagina y seccion cabañas

--- modelapp/views.py
+++ modelapp/views.py
@@ -1,12 +1,9 @@
 from django.shortcuts import render
 from modelapp.models import Cabaña
+
+
 def inicio(request):
-
-    return render(request,'index.html')
-
-
-def cabañas(request):
     cabañas = Cabaña.objects.all();
     dict = { "datos": cabañas}
     
-    return render(request,'cabañas.html',dict)
+    return render(request,'index.html',dict)
