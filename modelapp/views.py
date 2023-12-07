30e498a408021a9bd7e0c44d85e0b50d3d0089c3
tallerapp <grupotallerapp@gmail.com>
2023-12-07 18:32:54 -0300

    estilos de las cabañas

--- modelapp/views.py
+++ modelapp/views.py
@@ -1,9 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from modelapp.models import Cabaña
 
 
-def inicio(request):
+def index(request):
     cabañas = Cabaña.objects.all();
     dict = { "datos": cabañas}
     
     return render(request,'index.html',dict)
+
+def DetalleCabañas(request, cabaña_id):
+    cabaña = get_object_or_404(Cabaña, cabaña_id=cabaña_id)
+    return render(request, 'cabañas.html', {'cabaña': cabaña})
